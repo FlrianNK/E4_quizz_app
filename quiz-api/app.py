75d10d2240dd54a150dd46157127ebe1ef531394
TQNLP <thiquynh-nhu.pham@edu.esiee.fr>
2023-05-09 14:46:03 +0200

    first commit

---
+++ quiz-api/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def hello_world():
+    x = "World"
+    return f"Hello, {x}"
+
+
+if __name__ == "__main__":
+    app.run()
