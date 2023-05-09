85c55c2ebf9764db65379e99aa7f32a5cd62163b
TQNLP <thiquynh-nhu.pham@edu.esiee.fr>
2023-05-09 17:51:05 +0200

    create /quiz-info route

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -10,6 +10,10 @@ def hello_world():
     x = "World"
     return f"Hello, {x}"
 
+@app.route('/quiz-info', methods=['GET'])
+def GetQuizInfo():
+	return {"size": 0, "scores": []}, 200
+
 
 if __name__ == "__main__":
     app.run()
